421d3ad9b66a1c6ed687c83125828352e2daf39f
joshuaknight <g.joshuaknight01@gmail.com>
2016-05-12 11:43:21 -0400

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,13 +1,12 @@
 
 def fact():
 	user =  int(input('Enter the Number of Factorial:'))
-	i = 1
 	m= user
 	lis = []
 	for i in range(user):
 		lis.append(i * m )
 		user -= 1 
-		print lis
+	print lis
 
 
 fact()
